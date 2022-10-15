4d639058fc118c53fb285db415c34436f2e5cf15
duongloctai <duongloctai2206@gmail.com>
2022-10-15 15:05:22 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ contact.route.js
@@ -0,0 +1,19 @@
+const expess = require("expess");
+const contact = require("../cintroller/contact.controller");
+
+const router = expess.Router();
+
+router.route("/")
+.get(contacts.findAll)
+.post(contacts.create)
+.delete(contacts.deleteAll);
+
+router.route("/favorite")
+.get(contacts.findAllFavorite)
+
+router.route("/:id")
+.get(contacts.findOne)
+.put(contacts.update)
+.delete(contacts.delete);
+
+module.exports = router
